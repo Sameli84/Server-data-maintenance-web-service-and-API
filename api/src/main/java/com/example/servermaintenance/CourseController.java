17db966f1084ff15885c241c5cb83d056a347f98
Jere Penttinen <jere.penttinen@tuni.fi>
2022-05-05 13:30:33 +0300

    Add course creation (bug)

--- api/src/main/java/com/example/servermaintenance/CourseController.java
+++ api/src/main/java/com/example/servermaintenance/CourseController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -56,5 +57,21 @@ public class CourseController {
         courseRepository.save(course.get());
         accountRepository.save(account);
         return "redirect:/c/" + courseUrl;
+    }
+
+    @GetMapping("/create-course")
+    public String getCourseCreationPage() {
+        return "create_course";
+    }
+
+    @PostMapping("/create-course")
+    public String createCourse(@RequestParam String name, @RequestParam String url) {
+        var email = SecurityContextHolder.getContext().getAuthentication().getName();
+        var account = accountRepository.findByEmail(email).get();
+        var c = new Course(name, url,account);
+
+        // TODO: prevent duplicate urls
+        courseRepository.save(c);
+        return "redirect:/c/" + c.getUrl();
     }
 }
