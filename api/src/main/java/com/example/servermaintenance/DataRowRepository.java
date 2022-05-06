99549447b390a8292216b5f6d09d76ddcf6e2bc2
Jere Penttinen <jere.penttinen@tuni.fi>
2022-05-06 10:36:54 +0300

    Customize course page

--- api/src/main/java/com/example/servermaintenance/DataRowRepository.java
+++ api/src/main/java/com/example/servermaintenance/DataRowRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DataRowRepository extends JpaRepository<DataRow, Long> {
     List<DataRow> findAll();
+    List<DataRow> findDataRowsByCourse(Course course);
+    Optional<DataRow> findDataRowByCourseAndAccount(Course course, Account account);
 }
--- api/src/main/java/com/example/servermaintenance/DataRowService.java
+++ api/src/main/java/com/example/servermaintenance/DataRowService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DataRowService {
@@ -11,5 +12,15 @@ public class DataRowService {
     @Autowired
     private DataRowRepository dataRowRepository;
 
-    public List<DataRow> getDataRows() { return this.dataRowRepository.findAll(); }
+    public List<DataRow> getDataRows() {
+        return this.dataRowRepository.findAll();
+    }
+
+    public Optional<DataRow> getStudentData(Course course, Account account) {
+        return dataRowRepository.findDataRowByCourseAndAccount(course, account);
+    }
+
+    public List<DataRow> getCourseData(Course course) {
+        return dataRowRepository.findDataRowsByCourse(course);
+    }
 }
