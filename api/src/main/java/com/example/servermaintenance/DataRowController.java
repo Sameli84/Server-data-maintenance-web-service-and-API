ff7b20219e4230ee3d952590c1ee165f523dc412
Jere Penttinen <jere.penttinen@tuni.fi>
2022-05-06 13:07:56 +0300

    Rename endpoints to be according to rest

--- api/src/main/java/com/example/servermaintenance/DataRowController.java
+++ api/src/main/java/com/example/servermaintenance/DataRowController.java
@@ -18,49 +18,12 @@ public class DataRowController {
     private DataRowService dataRowService;
 
     @Autowired
-    private DataRowRepository dataRowRepository;
-
-    @Autowired
-    private CourseRepository courseRepository;
-
-    @Autowired
     private CourseService courseService;
-
-    @Autowired
-    private AccountRepository accountRepository;
-
-    @GetMapping("/bulkcreate")
-    public String bulkcreate(Authentication authentication) {
-// save a single Customer
-
-        var account = accountRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
-        if (account.isEmpty()) {
-            return "redirect:/login";
-        }
-
-        System.out.println(account.toString());
-
-        Course course = new Course("SoftaDevaus", "softa", account.get());
-        courseRepository.save(course);
-        dataRowRepository.save(new DataRow("Jakobi", "Juuseri", 55555, "theDNS", "myDNS", "Jaakko", "vpsJuuseri", "8.8.8.8", "123.123.124.12", account.get(), course));
-
-    /*
-        repository.saveAll(Arrays.asList(new DataRow("Salim", "Khan")
-                , new DataRow("Rajesh", "Parihar")
-                , new DataRow("Rahul", "Dravid")
-                , new DataRow("Dharmendra", "Bhojwani")));
-
-    */
-
-        return "redirect:/datarowpage";
-    }
 
     @GetMapping("/datarowpage")
     public String getDatarows(Model model) {
-        List<DataRow> dataRows = dataRowService.getDataRows();
-        List<Course> courses = courseService.getCourses();
-        model.addAttribute("datarows", dataRows);
-        model.addAttribute("courses", courses);
+        model.addAttribute("datarows", dataRowService.getDataRows());
+        model.addAttribute("courses", courseService.getCourses());
         return "datarowpage";
     }
 
@@ -78,29 +41,5 @@ public class DataRowController {
         model.addAttribute("datarows", dataRows);
         model.addAttribute("courses", courses);
         return "datarowpage";
-    }
-
-    @PostMapping("/createdata")
-    public String createData(@RequestParam String courseUrl, @RequestParam String studentAlias,
-                             @RequestParam String cscUsername, @RequestParam int uid,
-                             @RequestParam String dnsName, @RequestParam String selfMadeDnsName,
-                             @RequestParam String name, @RequestParam String vpsUsername,
-                             @RequestParam String poutaDns, @RequestParam String ipAddress, RedirectAttributes ra) {
-        var course = courseRepository.findCourseByUrl(courseUrl);
-        if (course.isEmpty()) {
-            // erroria?
-            return "redirect:/";
-        }
-        var email = SecurityContextHolder.getContext().getAuthentication().getName();
-        var account = accountRepository.findByEmail(email);
-        Boolean check = courseService.checkIfStudentOnCourse(course.get(), account.get());
-        if(!check) {
-            ra.addFlashAttribute("error", "You must sign up for course to create projects!");
-            return "redirect:/c/" + courseUrl;
-        }
-
-        dataRowRepository.save(new DataRow(studentAlias, cscUsername, uid, dnsName, selfMadeDnsName, name, vpsUsername, poutaDns, ipAddress, account.get(), course.get()));
-
-        return "redirect:/c/" + courseUrl;
     }
 }
