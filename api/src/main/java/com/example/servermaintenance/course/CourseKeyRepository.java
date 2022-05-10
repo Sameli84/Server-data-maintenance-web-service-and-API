3b82b2d5c10daeafa677218107cd5b98d17b72ec
Jere Penttinen <jere.penttinen@tuni.fi>
2022-05-10 14:46:13 +0300

    Add coursekeys

---
+++ api/src/main/java/com/example/servermaintenance/course/CourseKeyRepository.java
@@ -0,0 +1,13 @@
+package com.example.servermaintenance.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CourseKeyRepository extends JpaRepository<CourseKey, Long> {
+    List<CourseKey> findAll();
+    Optional<CourseKey> findCourseKeyByKey(String key);
+}
