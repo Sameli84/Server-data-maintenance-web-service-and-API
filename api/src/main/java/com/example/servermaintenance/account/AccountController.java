a90371120d16c8072569907e6568e3a73cc42596
JoonasTontti <joonas.tontti@tuni.fi>
2022-05-11 10:07:27 +0300

    Added email has to be @tuni.fi

--- api/src/main/java/com/example/servermaintenance/account/AccountController.java
+++ api/src/main/java/com/example/servermaintenance/account/AccountController.java
@@ -31,6 +31,11 @@ public class AccountController {
 
     @PostMapping("/register")
     public String signUp(@Valid @ModelAttribute Account account, BindingResult bindingResult, HttpServletRequest request) {
+        boolean emailVerified = account.getEmail().indexOf("@tuni.fi") != -1 ? true : false;
+        System.out.println("email "+ account.getEmail() +" verified: " + emailVerified);
+        if(emailVerified == false){
+            return "redirect:/register?error";
+        }
         if (bindingResult.hasErrors()) {
             return "register";
         }
