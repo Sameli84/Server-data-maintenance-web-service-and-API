5ce51a5f5525c96d1856f81f053d77923c87dee5
Sampsa Jarvinen <Sampsa.Jarvinen@tuni.fi>
2022-05-11 11:36:06 +0300

    Remove bulkcreate and a few other cleanings

--- api/src/main/java/com/example/servermaintenance/datarow/DataRowController.java
+++ api/src/main/java/com/example/servermaintenance/datarow/DataRowController.java
@@ -13,13 +13,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Controller
 public class DataRowController {
 
@@ -43,33 +40,6 @@ public class DataRowController {
 
     @Autowired
     private RoleService roleService;
-
-    @GetMapping("/bulkcreate")
-    public String bulkcreate(Authentication authentication) {
-// save a single Customer
-
-        var account = accountRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
-        if (account.isEmpty()) {
-            return "redirect:/login";
-        }
-
-        System.out.println(account.toString());
-
-        Course course = new Course("SoftaDevaus", "softa", account.get());
-        courseRepository.save(course);
-        dataRowRepository.save(new DataRow("Jakobi", "Juuseri", 55555, "theDNS", "myDNS", "Jaakko", "vpsJuuseri", "8.8.8.8", "123.123.124.12", account.get(), course));
-
-    /*
-        repository.saveAll(Arrays.asList(new DataRow("Salim", "Khan")
-                , new DataRow("Rajesh", "Parihar")
-                , new DataRow("Rahul", "Dravid")
-                , new DataRow("Dharmendra", "Bhojwani")));
-
-    */
-
-        return "redirect:/datarowpage";
-    }
-
     @Secured("ROLE_TEACHER")
     @GetMapping("/datarowpage")
     public String getDatarows(Model model, @RequestParam Optional<Long> selectCourse) {
