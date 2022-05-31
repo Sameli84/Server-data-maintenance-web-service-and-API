474993ea6f04d5d449d6374f2fd43e7aca3409b7
Jere Penttinen <jere.penttinen@tuni.fi>
2022-05-31 15:36:04 +0300

    Switch interpreter to builder pattern

--- api/src/main/java/com/example/servermaintenance/interpreter/Interpreter.java
+++ api/src/main/java/com/example/servermaintenance/interpreter/Interpreter.java
@@ -1,24 +1,41 @@
 package com.example.servermaintenance.interpreter;
 
+import com.example.servermaintenance.interpreter.ast.Program;
 import com.example.servermaintenance.interpreter.evaluator.Environment;
 import com.example.servermaintenance.interpreter.evaluator.Evaluator;
 import com.example.servermaintenance.interpreter.lexer.Lexer;
 import com.example.servermaintenance.interpreter.parser.Parser;
 
 public class Interpreter {
-    public static String eval(String statement, long id) {
-        var lexer = new Lexer(statement);
-        var parser = new Parser(lexer);
+    private final Environment environment = new Environment();
+    private final Program program;
+    private final Parser parser;
 
-        var program = parser.parseProgram();
+    public Interpreter(final String statement) {
+        this.parser = new Parser(new Lexer(statement));
+        this.program = parser.parseProgram();
+    }
 
+    public Interpreter declareInt(final String varName, int value) {
+        this.environment.putInteger(varName, value);
+        return this;
+    }
+
+    public Interpreter declareLong(final String varName, long value) {
+        this.environment.putInteger(varName, (int) value);
+        return this;
+    }
+
+    public Interpreter declareString(final String varName, final String value) {
+        this.environment.putString(varName, value);
+        return this;
+    }
+
+    public String execute() {
         if (parser.getErrors().size() != 0) {
             return parser.getErrors().get(0);
         }
 
-        var env = new Environment();
-        env.putInteger("id", (int)id);
-        var evaluated = Evaluator.eval(program, env);
-        return evaluated.toString();
+        return Evaluator.eval(program, environment).toString();
     }
 }
