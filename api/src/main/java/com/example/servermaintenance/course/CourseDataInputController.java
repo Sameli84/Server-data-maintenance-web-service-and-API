0fc42eff3e29fad1da58144f50137af2985f87bd
Jere Penttinen <jere.penttinen@tuni.fi>
2022-06-07 14:59:15 +0300

    Protect data input page

--- api/src/main/java/com/example/servermaintenance/course/CourseDataInputController.java
+++ api/src/main/java/com/example/servermaintenance/course/CourseDataInputController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -56,25 +57,31 @@ public class CourseDataInputController {
         return new CourseSessionMap<>();
     }
 
+    @ModelAttribute("canEdit")
+    public boolean addCanEditToModel(@ModelAttribute Course course, @ModelAttribute Account account) {
+        return Objects.equals(account.getId(), course.getOwner().getId()) || roleService.isAdmin(account);
+    }
+
     @ModelAttribute("courseDataInputDto")
     public CourseDataInputDto addCourseDataInputDtoToModel(@ModelAttribute CourseSessionMap<CourseDataInputDto> courseSessionMap,
                                                            @ModelAttribute Course course) {
         return courseSessionMap.getOrDefault(course, () -> courseService.getCourseDataForm(course));
     }
 
-    @ModelAttribute("isStudent")
-    public boolean addIsStudentToModel(@ModelAttribute Course course, @ModelAttribute Account account) {
-        return courseService.isStudentOnCourse(course, account);
-    }
-
-    @ModelAttribute("canEdit")
-    public boolean addCanEditToModel(@ModelAttribute Course course, @ModelAttribute Account account) {
-        return Objects.equals(account.getId(), course.getOwner().getId()) || roleService.isAdmin(account);
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    private boolean canEdit(Model model) {
+        var canEdit = (Boolean) model.getAttribute("canEdit");
+        if (canEdit == null) {
+            return false;
+        }
+        return canEdit;
     }
 
     @GetMapping
-    public String getDataTab(@PathVariable Course course, @ModelAttribute CourseDataInputDto courseDataInputDto, Model model) {
-        if (courseDataInputDto.isEdit()) {
+    public String getDataTab(@PathVariable Course course,
+                             @ModelAttribute CourseDataInputDto courseDataInputDto,
+                             Model model) {
+        if (canEdit(model) && courseDataInputDto.isEdit()) {
             return "course/tab-data-edit";
         } else {
             model.addAttribute("courseData", courseService.getCourseData(course));
@@ -87,6 +94,9 @@ public class CourseDataInputController {
                             @ModelAttribute CourseSessionMap<CourseDataInputDto> courseSessionMap,
                             @ModelAttribute CourseDataInputDto courseDataInputDto,
                             Model model) {
+        if (!canEdit(model)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized action");
+        }
         courseService.saveCourseDataInput(courseDataInputDto);
         model.addAttribute("courseData", courseDataInputDto.getCourseDataDto());
         courseSessionMap.remove(course);
@@ -95,13 +105,23 @@ public class CourseDataInputController {
     }
 
     @PostMapping("/edit")
-    public String setEdit(@PathVariable Course course, @ModelAttribute CourseDataInputDto courseDataInputDto, Model model) {
+    public String setEdit(@PathVariable Course course,
+                          @ModelAttribute CourseDataInputDto courseDataInputDto,
+                          Model model) {
+        if (!canEdit(model)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized action");
+        }
         courseDataInputDto.setEdit(true);
         return getDataTab(course, courseDataInputDto, model);
     }
 
     @PostMapping("/cancel")
-    public String cancelEdits(@PathVariable Course course, @ModelAttribute CourseSessionMap<CourseDataInputDto> courseSessionMap, Model model) {
+    public String cancelEdits(@PathVariable Course course,
+                              @ModelAttribute CourseSessionMap<CourseDataInputDto> courseSessionMap,
+                              Model model) {
+        if (!canEdit(model)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized action");
+        }
         var courseDataInputDto = courseSessionMap.add(course, courseService.getCourseDataForm(course));
         return getDataTab(course, courseDataInputDto, model);
     }
@@ -112,7 +132,12 @@ public class CourseDataInputController {
                             @PathVariable int row,
                             @PathVariable int part,
                             @RequestParam String value,
-                            @ModelAttribute CourseDataInputDto courseDataInputDto) {
+                            @ModelAttribute CourseDataInputDto courseDataInputDto,
+                            Model model) {
+        if (!canEdit(model)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized action");
+        }
+
         var parts = courseDataInputDto.getCourseDataDto().getRows().get(row).getParts();
 
         var studentPart = parts.get(part);
