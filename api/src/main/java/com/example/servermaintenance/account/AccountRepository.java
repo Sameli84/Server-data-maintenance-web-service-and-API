516d0abf8af85c08c5548ebe8a219cf1c7343a28
Sampsa Jarvinen <Sampsa.Jarvinen@tuni.fi>
2022-06-17 08:48:15 +0300

    Fix admin-tools view to not lose accounts with no roles

--- api/src/main/java/com/example/servermaintenance/account/AccountRepository.java
+++ api/src/main/java/com/example/servermaintenance/account/AccountRepository.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Long> {
     @Query("select a from Account a join fetch a.roles where a.email = :email")
     Optional<Account> findByEmail(String email);
-    @Query("select a from Account a join fetch a.roles where lower(a.email) like lower(concat('%', :email, '%'))")
+    @Query("select distinct a from Account a left join fetch a.roles where lower(a.email) like lower(concat('%', :email, '%'))")
     List<Account> findAccountsByEmailContainingIgnoreCase(String email);
 }
