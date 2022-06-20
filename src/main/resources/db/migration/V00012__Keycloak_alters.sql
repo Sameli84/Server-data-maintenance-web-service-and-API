da3bf8ecb0e2bfb9cd9aea496128f9c3a79ec3ce
Jere Penttinen <jere.penttinen@tuni.fi>
2022-06-20 08:37:43 +0300

    Quick fix 12 migration

--- src/main/resources/db/migration/V00012__Keycloak_alters.sql
+++ src/main/resources/db/migration/V00012__Keycloak_alters.sql
@@ -4,11 +4,8 @@ alter table account
 alter table account
     add column keycloak_uuid uuid;
 
-alter table account
-    drop column name;
-
 create index on account (keycloak_uuid);
 
-drop table account_roles;
-drop table role;
+drop table if exists account_roles;
+drop table if exists role;
 drop table if exists student_course;
